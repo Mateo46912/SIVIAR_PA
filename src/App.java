91cca973ddb8516caaac53d285db11933d077d8f
Mateo Cordero <mateo.cor45@gmail.com>
2026-01-04 16:00:02 -0500

    Primer respaldo del proyecto SIVAR

---
+++ src/App.java
@@ -0,0 +1,9 @@
+import Vista.AnteProyecto;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+          AnteProyecto vista = new AnteProyecto();
+          vista.setVisible(true);
+
+    }
+}
---
+++ src/Arduino/ArduinoService.java
@@ -0,0 +1,5 @@
+package Arduino;
+
+public class ArduinoService {
+    
+}
